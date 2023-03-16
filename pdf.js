76cb41e812096ff7a2a68857ad5dfc35043de85c
Adnan Khan <Ghories555@gmail.com>
2023-03-16 21:46:50 +0100

    Add zoom in/out, next/previous page functionality, and improve highlighter transparency
    - Implement zoom in/out functionality with buttons in the toolbar
    - Add next and previous page navigation for multi-page PDFs
    - Adjust highlighter color to be semi-transparent for better visibility
    - Update styling for toolbar separation and initial message display

--- pdf.js
+++ pdf.js
@@ -3,25 +3,33 @@ const canvas = document.getElementById('pdf-canvas');
 const context = canvas.getContext('2d');
 const annotationCanvas = document.getElementById('annotation-canvas');
 const annotationContext = annotationCanvas.getContext('2d');
+const CanvasWrapper = document.getElementById('canvas-wrapper');
 
 const penButton = document.getElementById('pen');
 const highlighterButton = document.getElementById('highlighter');
 const eraserButton = document.getElementById('eraser');
 const saveButton = document.getElementById('save');
+const zoomInButton = document.getElementById('zoom-in');
+const zoomOutButton = document.getElementById('zoom-out');
+const prevPageButton = document.getElementById('prev-page');
+const nextPageButton = document.getElementById('next-page');
+
+const noPdfMessage = document.getElementById('no-pdf-message');
+
 
 let pdfDoc = null;
 let pageNum = 1;
 let tool = 'pen';
 let drawing = false;
 let annotations = [];
-
+let scale = 1;
 
 annotationCanvas.width = canvas.width;
 annotationCanvas.height = canvas.height;
 
 const openPdfButton = document.getElementById('open-pdf');
 openPdfButton.addEventListener('click', () => {
-  fileInput.click();
+    fileInput.click();
 });
 
 fileInput.addEventListener('change', async (e) => {
@@ -30,6 +38,8 @@ fileInput.addEventListener('change', async (e) => {
         alert('Please select a PDF file.');
         return;
     }
+
+    noPdfMessage.style.display = 'none';
 
     const reader = new FileReader();
     reader.onload = async (e) => {
@@ -40,6 +50,32 @@ fileInput.addEventListener('change', async (e) => {
     reader.readAsArrayBuffer(file);
 });
 
+
+
+zoomInButton.addEventListener('click', () => {
+    scale += 0.25;
+    renderPDF(pdfData);
+    console.log(scale);
+});
+
+zoomOutButton.addEventListener('click', () => {
+    scale = Math.max(0.5, scale - 0.25);
+    renderPDF(pdfData);
+});
+
+prevPageButton.addEventListener('click', () => {
+    if (pageNum <= 1) return; // Prevent going below the first page
+    pageNum--;
+    renderPDF(pdfData);
+});
+
+nextPageButton.addEventListener('click', async () => {
+    if (!pdfData) return; // Prevent going beyond the last page if pdfData is not available
+    const numPages = pdfData.numPages;
+    if (pageNum >= numPages) return;
+    pageNum++;
+    renderPDF(pdfData);
+});
 
 
 
@@ -79,7 +115,7 @@ annotationCanvas.addEventListener('mouseup', () => drawing = false);
 
 async function renderPDF(pdfDoc) {
     const page = await pdfDoc.getPage(pageNum);
-    const viewport = page.getViewport({ scale: 1 });
+    const viewport = page.getViewport({ scale });
 
     canvas.width = viewport.width;
     canvas.height = viewport.height;
@@ -91,6 +127,12 @@ async function renderPDF(pdfDoc) {
         viewport: viewport,
     };
     await page.render(renderContext).promise;
+
+    // Show the canvases when a PDF is loaded
+    CanvasWrapper.style.display = 'inline-block';
+
+    // Hide the no-pdf-message when a PDF is loaded
+    noPdfMessage.style.display = 'none';
 }
 
 
@@ -100,7 +142,7 @@ function renderAnnotations() {
     for (const annotation of annotations) {
         const { tool, points } = annotation;
         annotationContext.lineWidth = tool === 'pen' ? 1 : 10;
-        annotationContext.strokeStyle = tool === 'highlighter' ? 'yellow' : 'black';
+        annotationContext.strokeStyle = tool === 'highlighter' ? 'rgba(255, 255, 0, 0.5)' : 'black';
         annotationContext.globalCompositeOperation = tool === 'eraser' ? 'destination-out' : 'source-over';
         annotationContext.beginPath();
         annotationContext.moveTo(points[0].x, points[0].y);
@@ -132,12 +174,19 @@ function eraseAnnotations(x, y, radius) {
 window.jsPDF = window.jspdf.jsPDF;
 
 saveButton.addEventListener('click', async () => {
+    const exportScale = 4; // Increase this value to improve the quality (e.g., 2, 3, or 4)
     const tempCanvas = document.createElement('canvas');
-    tempCanvas.width = canvas.width;
-    tempCanvas.height = canvas.height;
+    tempCanvas.width = canvas.width * exportScale;
+    tempCanvas.height = canvas.height * exportScale;
     const tempContext = tempCanvas.getContext('2d');
+    tempContext.scale(exportScale, exportScale);
     await renderPDF(pdfData);
     tempContext.drawImage(canvas, 0, 0);
+
+    if (!pdfData) {
+        noPdfMessage.style.display = 'block';
+        return;
+    }
 
     // Draw the annotations on the temporary canvas
     for (const annotation of annotations) {
@@ -159,6 +208,7 @@ saveButton.addEventListener('click', async () => {
     pdf.addImage(tempCanvas.toDataURL('image/png'), 'PNG', 0, 0, tempCanvas.width, tempCanvas.height);
     pdf.save('annotated.pdf');
 });
+
 
 
 
